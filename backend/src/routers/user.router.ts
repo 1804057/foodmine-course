5a4d02aa2c4d72aea6f526ff240e54cf0f30b59d
1804057 <u1804057@student.cuet.ac.bd>
2023-10-29 16:16:40 +0600

    Login added with MongoDb

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -18,9 +18,10 @@ router.get("/seed", asyncHandler(
  }
  ))
 
-router.post("/login", (req, res)=>{
+router.post("/login", asyncHandler(
+  async (req, res)=>{
     const {email, password} = req.body;
-    const user = sample_users.find(user=>user.email===email && user.password===password);
+    const user = await UserModel.findOne({email,password})
   
     if(user){
       res.send(generateTokenReponse(user)); //successful response sending to the client
@@ -30,7 +31,8 @@ router.post("/login", (req, res)=>{
       res.status(400).send("Username or password is not found")
     }
   
-  })
+  }
+))
   
   const generateTokenReponse = (user : any) => {
     const token = jwt.sign({ //precosee of generating token is called signing
