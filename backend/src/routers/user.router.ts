19dd73b999b8a5a16357368146f84bf4d1c88dfc
1804057 <u1804057@student.cuet.ac.bd>
2023-11-01 00:34:08 +0600

    create order code written, but buged.

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -66,7 +66,7 @@ router.post('/register', asyncHandler(
   
   const generateTokenReponse = (user : any) => {
     const token = jwt.sign({ //precosee of generating token is called signing
-      email:user.email, isAdmin: user.isAdmin
+      id: user.id, email:user.email, isAdmin: user.isAdmin
     },"SomeRandomText",{
       expiresIn:"30d"
     });
